9aa7a83559475641a5ede52b1ef784fe35da3924
fauziap <ujiahmadfauzi05@gmail.com>
2024-08-28 21:58:04 +0700

    update foto logoo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {logo} from "./assets/logo.svg";
+import logo from "../assets/logo.svg";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
